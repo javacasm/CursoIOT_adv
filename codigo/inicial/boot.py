474c95bc2f87564ad8c62af369948d20bcc66f38
javacasm <javacasm@gmail.com>
2022-03-25 12:59:31 +0100

    updae

--- codigo/inicial/boot.py
+++ codigo/inicial/boot.py
@@ -7,18 +7,23 @@ import time
 import machine
 import blinking 
 
+
+SSID = 'OpenWrt'
+WIFI_PASSWD = 'qazxcvbgtrewsdf'
+
 w = network.WLAN(network.STA_IF)
 if not w.active(): # evita el error OSError de intentar activar si ya activa
     w.active(True)  
     
 if not w.isconnected():
-    w.connect('OpenWrt','qazxcvbgtrewsdf')
+    print('Conectando a ',SSID)
+    w.connect(SSID, WIFI_PASSWD)
     while not w.isconnected():
         print('.', end='')
         blinking.parpadeo(tiempo_encendido=100,tiempo_apagado=100)
         time.sleep(0.1)
         
-print('IP:',w.ifconfig()[0]) # imprimimos sólo el primer elemento que es la ip
+print('Conectado con IP:',w.ifconfig()[0]) # imprimimos sólo el primer elemento que es la ip
 
 #import webrepl
 #webrepl.start()
